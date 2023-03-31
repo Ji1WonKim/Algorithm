bb55645142771c3029ea7346113574d91b6981bb
JiseonKimm <118652915+JiseonKimm@users.noreply.github.com>
2023-04-01 00:25:32 +0900

    Add files via upload

---
+++ bfs ex1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+#bfs 메서드 정의
+def bfs(graph, start, visited):
+    queue = deque([start])
+    
+    visited[start] = True
+    while queue:
+        #큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        #아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+    graph = [
+[],      
+[2,3,8],
+[1,7],
+[1,4,5],
+[3,5],
+[3,4],
+[7],
+[2,6,8],
+[1,7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
+]
